efcb07bd3390f7e2951ed482394af752bd8cfb58
Cody Pizzaia <cpizzaia@asu.edu>
2016-02-28 17:18:58 -0500

    add domain and session urls

--- app/actions/actionCreators.js
+++ app/actions/actionCreators.js
@@ -1,18 +1,22 @@
 import { RECEIVE_CURRENT_USER } from '../constants/actionTypes'
 
+const DOMAIN_URL = "http://bartrspace.herokuapp.com/api"
+
+const SESSION_URL = "/session"
+
 const receiveCurrentUser = (current_user) => {
   return { type: RECEIVE_CURRENT_USER, current_user }
 }
 
 export const fetchCurrentUser = () => {
   return dispatch => {
-    fetch('http://www.ciderspot.com/api/ciders')
+    fetch(DOMAIN_URL + SESSION_URL)
       .then((response) => response.json())
       .then((responseText) => {
         dispatch(receiveCurrentUser(responseText));
       })
       .catch((error) => {
-        receiveCurrentUser();
+        receiveCurrentUser({});
       });
   }
 }
--- app/reducers/users.js
+++ app/reducers/users.js
@@ -3,7 +3,7 @@ import { RECEIVE_CURRENT_USER } from "../constants/actionTypes"
 export default function users(state = {}, action) {
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      return "test"
+      return action.current_user
     default:
       return state
   }
